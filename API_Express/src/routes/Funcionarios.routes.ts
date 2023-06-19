42d4fb7c6063c1de3a943d94de1949ae788706b6
Josué Lustosa <josuelustosa.job@gmail.com>
2023-06-19 09:09:40 -0400

    feat: finalizando rotas de funcionarios

--- API_Express/src/routes/Funcionarios.routes.ts
+++ API_Express/src/routes/Funcionarios.routes.ts
@@ -8,7 +8,50 @@ funcionariosRouter.get(
   async (req: Request, res: Response): Promise<Response> => {
     const funcionarios: Funcionarios[] = await Funcionarios.findAll();
     const quantidadeFuncionarios: number = await Funcionarios.count();
-    return res.status(200).json({quantidadeFuncionarios, funcionarios});
+    return res.status(200).json({ quantidadeFuncionarios, funcionarios });
+  }
+);
+
+funcionariosRouter.get(
+  "/funcionarios/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    const funcionarios: Funcionarios | null = await Funcionarios.findByPk(id);
+    return res.status(200).json(funcionarios);
+  }
+);
+
+funcionariosRouter.post(
+  "/funcionarios",
+  async (req: Request, res: Response): Promise<Response> => {
+    const funcionarios: Funcionarios = await Funcionarios.create({
+      ...req.body,
+    });
+    return res.status(201).json(funcionarios);
+  }
+);
+
+funcionariosRouter.put(
+  "/funcionarios/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    await Funcionarios.update({ ...req.body }, { where: { id } });
+    const updatedFuncionario: Funcionarios | null = await Funcionarios.findByPk(
+      id
+    );
+    return res.status(200).json(updatedFuncionario);
+  }
+);
+
+funcionariosRouter.delete(
+  "/funcionarios/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    const { id } = req.params;
+    const deletedFuncionario: Funcionarios | null = await Funcionarios.findByPk(
+      id
+    );
+    await Funcionarios.destroy({ where: { id } });
+    return res.status(200).json(deletedFuncionario);
   }
 );
 
